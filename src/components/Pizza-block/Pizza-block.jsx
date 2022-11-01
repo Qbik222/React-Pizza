db4482a079fc4e66fd04c38530c1b32b47e3952a
Qbik222 <97127218+Qbik222@users.noreply.github.com>
2022-11-01 15:37:37 +0200

    add pizza-block items

--- src/components/Pizza-block/Pizza-block.jsx
+++ src/components/Pizza-block/Pizza-block.jsx
@@ -1,51 +1,49 @@
-import { React, useState } from 'react';
+import React from 'react';
 
 import './pizza-block.scss';
 
 function PizzaBlock(props) {
-  const [num, setOfNum] = useState(0);
+  // const [num, setOfNum] = useState(0);
 
-  const calcNum = () => {
-    setOfNum((num) => num + 1);
-  };
+  // const calcNum = () => {
+  //   setOfNum((num) => num + 1);
+  // };
 
-  const { title, price } = props;
+  // console.log(pizzas.map((item) => console.log(item)));
+
+  const { id, imageUrl, title, sizes, price } = props;
 
   return (
-    <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
-      <h4 className="pizza-block__title">{title}</h4>
-      <div className="pizza-block__selector">
+    <div className='pizza-block' key={id}>
+      <img className='pizza-block__image' src={imageUrl} alt='Pizza' />
+      <h4 className='pizza-block__title'>{title}</h4>
+      <div className='pizza-block__selector'>
         <ul>
-          <li className="active">тонкое</li>
+          <li className='active'>тонкое</li>
           <li>традиционное</li>
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((item) => {
+            return <li key={id}>{item}</li>;
+          })}
         </ul>
       </div>
-      <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от {price} ₽</div>
-        <div onClick={calcNum} className="button button--outline button--add">
+      <div className='pizza-block__bottom'>
+        <div className='pizza-block__price'>от {price} ₴</div>
+        <div className='button button--outline button--add'>
           <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
+            width='12'
+            height='12'
+            viewBox='0 0 12 12'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'>
             <path
-              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-              fill="white"
+              d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
+              fill='white'
             />
           </svg>
           <span>Добавить</span>
-          <i>{num}</i>
+          <i>0</i>
         </div>
       </div>
     </div>
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,20 +4,36 @@ import Categories from '../Categories/Categories';
 import Sort from '../Sort/Sort';
 import PizzaBlock from '../Pizza-block/Pizza-block';
 import './app.scss';
+import pizzas from '../../assets/pizzas.json';
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className='wrapper'>
       <Header />
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
             <Categories />
             <Sort />
           </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            <PizzaBlock title="Мексиканская" price="300" />
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+            {pizzas.map((obj) => {
+              const { id, imageUrl, title, types, sizes, price, category, raiting } = obj;
+
+              return (
+                <PizzaBlock
+                  id={id}
+                  imageUrl={imageUrl}
+                  title={title}
+                  types={types}
+                  sizes={sizes}
+                  price={price}
+                  category={category}
+                  raiting={raiting}
+                />
+              );
+            })}
           </div>
         </div>
       </div>

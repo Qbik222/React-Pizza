3fbe492dec3480122fcd629d15567b69391aba7c
Qbik222 <97127218+Qbik222@users.noreply.github.com>
2022-11-02 14:17:23 +0200

    add db on mokapi.io

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import Categories from '../Categories/Categories';
 import Sort from '../Sort/Sort';
 import PizzaBlock from '../Pizza-block/Pizza-block';
 import './app.scss';
-import pizzas from '../../assets/pizzas.json';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://63625e1e66f75177ea2d7159.mockapi.io/items')
+      .then((res) => res.json())
+      .then((res) => setItems(res));
+  }, []);
+
   return (
     <div className='wrapper'>
       <Header />
@@ -18,7 +25,7 @@ function App() {
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            {pizzas.map((obj) => {
+            {items.map((obj) => {
               return <PizzaBlock {...obj} key={obj.id} />;
             })}
           </div>
